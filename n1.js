cc0edd6ec6087f61573623bd2df4cbd54322f868
Rahuljamod28 <jamodrahul72234@gmail.com>
2026-02-01 14:50:50 +0530

    currency converter

---
+++ n1.js
@@ -0,0 +1,75 @@
+// const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
+
+// const dropdowns = document.querySelectorAll(".dropdown select");
+
+// for(let select of dropdowns) {
+//     for(currCode in countryList) {
+//         let newoption = document.createElement("option");
+//         newoption.value = currCode;
+//         select.append(newoption);
+//     }
+// }
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.value = currCode;
+        newOption.innerText = currCode;
+
+        // default selection
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = true;
+        }
+        if (select.name === "to" && currCode === "INR") {
+            newOption.selected = true;
+        }
+
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+
+    const URL = `${BASE_URL}/${fromCurr.value}`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    // correct rate access
+    let rate = data.rates[toCurr.value];
+    let finalAmount = amtVal * rate;
+
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+});
+
